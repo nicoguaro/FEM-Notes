a38d7ce752828deb3647bfd7e468fbefc7fc9af9
nicoguaro <nicoguarin@gmail.com>
2015-08-12 18:29:44 -0400

    Change spring figures and script
    - Updated spring figures to Inkscape files.
    - Polished script for spring system
      - It is still pending to document how to use it and generate
        some tests.

--- scripts/SPRINGS/femutil.py
+++ scripts/SPRINGS/femutil.py
@@ -1,22 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Feb 17 10:02:49 2015
+Utilities for FEM computations
 
-@author: eafit
 """
 import numpy as np
-def mvprod(a,b,irow,icol):
-    c=np.zeros(irow)
-    for i in range(0,irow):
-        c[i]=0.0
-        for j in range(0,icol):
-            c[i]=c[i]+a[i,j]*b[j]
-    return c
-#
+
 def uel(k):
-    kl=np.zeros([2,2],dtype=np.float128)
-    kl[0,0]= k
-    kl[0,1]=-k
-    kl[1,0]=-k
-    kl[1,1]= k    
+    """Return stiffness matrix for a spring with constant `k`"""
+    kl = np.zeros([2,2], dtype=float)
+    kl[0, 0] = k
+    kl[0, 1] = -k
+    kl[1, 0] = k
+    kl[1, 1] = k
+    
     return kl    
